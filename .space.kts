68810a1a535d3ddd777cb45227cc7539e765903f
Louis FORAUX <louis@sledgee.com>
2022-10-16 21:15:13 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,21 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Run npm test") {
+    container(displayName = "Run publish script", image = "node:14-alpine") {
+        shellScript {
+            interpreter = "/bin/sh"
+            content = """
+                echo Install npm dependencies...
+                npm ci
+                echo Run build if it exists in package.json...
+                npm run build --if-present
+                echo Run tests...
+                npm run test
+            """
+        }
+    }
+}
